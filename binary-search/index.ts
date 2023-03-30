8f2415eef74836cd3c28ead12846ac8daba6ff58
Rafael Melo <rafael.melo@curseduca.com>
2023-03-30 11:37:07 -0300

    improv: implementing binary search algo

---
+++ binary-search/index.ts
@@ -0,0 +1,33 @@
+// O(log n)
+// O(log 32)
+
+function binarySearch(list: number[], target: number): number | null {
+	let executions = 0;
+
+	let start = 0;
+	let end = list.length - 1;
+
+	while (start <= end) {
+		console.log(`EXECUTION: ${executions++}`);
+
+		const middle = Math.floor((start + end) / 2);
+
+		const element = list[middle];
+
+		if (target < element) {
+			end = middle - 1;
+		} else if (target > element) {
+			start = middle + 1;
+		} else {
+			return list[middle];
+		}
+	}
+
+	return null;
+}
+
+const orderedList = Array.from({ length: 32 }, (_, i) => i + 1);
+const toFind = 31;
+
+const result = binarySearch(orderedList, toFind);
+console.log(`RESULT: ${result}`);
